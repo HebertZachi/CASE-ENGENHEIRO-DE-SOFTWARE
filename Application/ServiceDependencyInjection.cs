ca13f1273408f6c4f5a6fdf25ccae02d667ea8d7
Hebert Zachi <hebertzachi.contato@outlook.com>
2026-01-28 10:04:16 -0300

    :wrench: chore: add di setup for services

---
+++ Application/ServiceDependencyInjection.cs
@@ -0,0 +1,22 @@
+﻿using Application.Services;
+using Application.Services.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Application
+{
+    public static class ServiceDependencyInjection
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            services.AddScoped<IPessoaFisicaService, PessoaFisicaService>();
+            services.AddScoped<IPessoaJuridicaService, PessoaJuridicaService>();
+            services.AddScoped<IEnderecoService, EnderecoService>();
+            services.AddScoped<IPessoaFisicaEnderecoService, PessoaFisicaEnderecoService>();
+
+            return services;
+        }
+    }
+}
