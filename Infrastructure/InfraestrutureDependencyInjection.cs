99182a290475cbf71fc8dc0705ee721dc32e234e
Hebert Zachi <hebertzachi.contato@outlook.com>
2026-01-28 09:41:02 -0300

    :wrench: chore: add dependency injection setup for repositories

---
+++ Infrastructure/InfraestrutureDependencyInjection.cs
@@ -0,0 +1,30 @@
+﻿using Infrastructure.Persistence;
+using Infrastructure.Repositories;
+using Infrastructure.Repositories.Interfaces;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure
+{
+    public static class InfraestrutureDependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(
+            this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            services.AddDbContext<AppDbContext>(options =>
+                options.UseNpgsql(
+                    configuration.GetConnectionString("DefaultConnection")
+                )
+            );
+
+            services.AddScoped<IEnderecoRepository, EnderecoRepository>();
+            services.AddScoped<IPessoaFisicaRepository, PessoaFisicaRepository>();
+            services.AddScoped<IPessoaJuridicaRepository, PessoaJuridicaRepository>();
+            services.AddScoped<IPessoaFisicaEnderecoRepository, PessoaFisicaEnderecoRepository>();
+
+            return services;
+        }
+    }
+}
