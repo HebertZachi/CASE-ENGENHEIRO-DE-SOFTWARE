084f1e29d310fe540ef8e574e5a74214cb53cfb6
Hebert Zachi <hebertzachi.contato@outlook.com>
2026-02-02 12:55:28 -0300

    :sparkles: feat: added viacep service interface for external service adapters

---
+++ Application.Interfaces/ExternalServices/IViaCepService.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace Infrastructure.Repositories.Interfaces
+{
+    public interface IViaCepService
+    {
+        Task<Endereco?> FindAddressByCep(string cep);
+    }
+}
