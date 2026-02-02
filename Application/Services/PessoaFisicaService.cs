c526f66af2c11ec20147461a3c40e4816d560aa0
Hebert Zachi <hebertzachi.contato@outlook.com>
2026-02-02 19:15:45 -0300

    :recycle: refactor: added structured logs to service error handling

--- Application/Services/PessoaFisicaService.cs
+++ Application/Services/PessoaFisicaService.cs
@@ -2,21 +2,26 @@
 using Application.Services.Interfaces;
 using Domain.Entities;
 using Infrastructure.Repositories.Interfaces;
+using Microsoft.Extensions.Logging;
 
 namespace Application.Services
 {
     public class PessoaFisicaService : BaseService<PessoaFisica>, IPessoaFisicaService
     {
+        private readonly ILogger<PessoaFisicaService> _logger;
         private readonly IPessoaFisicaRepository _repository;
         private readonly IViaCepService _viaCepService;
 
         public PessoaFisicaService(
+            ILogger<PessoaFisicaService> logger,
             IPessoaFisicaRepository repository,
-            IViaCepService viaCepService)
+            IViaCepService viaCepService
+            )
             : base(repository)
         {
             _repository = repository;
             _viaCepService = viaCepService;
+            _logger = logger;
         }
 
         public PessoaFisicaService(IPessoaFisicaRepository repository)
@@ -26,154 +31,186 @@ namespace Application.Services
         }
         public async Task<PessoaFisicaDto?> FindByCpf(string cpf)
         {
-            var pessoaFisica = await _repository.FindByCpf(cpf);
+            try
+            {
+                var pessoaFisica = await _repository.FindByCpf(cpf);
 
-            if (pessoaFisica == null)
-                return null;
+                if (pessoaFisica == null)
+                    return null;
 
-            var enderecosDto = pessoaFisica.Enderecos
-                .Select(pe => new EnderecoDto(
-                    pe.Endereco.Id,
-                    pe.Endereco.Cep,
-                    pe.Endereco.Logradouro,
-                    pe.Endereco.Complemento,
-                    pe.Endereco.Unidade,
-                    pe.Endereco.Bairro,
-                    pe.Endereco.Localidade,
-                    pe.Endereco.Uf,
-                    pe.Endereco.Estado,
-                    pe.Endereco.Regiao,
-                    pe.Endereco.Ddd
-                ))
-                .ToList();
+                var enderecosDto = pessoaFisica.Enderecos
+                    .Select(pe => new EnderecoDto(
+                        pe.Endereco.Id,
+                        pe.Endereco.Cep,
+                        pe.Endereco.Logradouro,
+                        pe.Endereco.Complemento,
+                        pe.Endereco.Unidade,
+                        pe.Endereco.Bairro,
+                        pe.Endereco.Localidade,
+                        pe.Endereco.Uf,
+                        pe.Endereco.Estado,
+                        pe.Endereco.Regiao,
+                        pe.Endereco.Ddd
+                    ))
+                    .ToList();
 
-            return new PessoaFisicaDto(
-                pessoaFisica.Id,
-                pessoaFisica.Nome,
-                pessoaFisica.Sobrenome,
-                pessoaFisica.Cpf,
-                pessoaFisica.DataNascimento,
-                enderecosDto
-            );
+                return new PessoaFisicaDto(
+                    pessoaFisica.Id,
+                    pessoaFisica.Nome,
+                    pessoaFisica.Sobrenome,
+                    pessoaFisica.Cpf,
+                    pessoaFisica.DataNascimento,
+                    enderecosDto
+                );
+
+            } catch (Exception ex)
+            {
+                _logger.LogError($"Error to find pessoa física by CPF: {cpf}", ex);
+                throw;
+            }
         }
 
         public async Task<IEnumerable<PessoaFisicaDto?>> FindByName(string name, int page = 1, int limit = 10)
         {
-            var pessoasFisica = await _repository.FindByName(name, page, limit);
+            try
+            {
+                var pessoasFisica = await _repository.FindByName(name, page, limit);
 
-            if (pessoasFisica == null)
-                return null;
+                if (pessoasFisica == null)
+                    return null;
 
-            return pessoasFisica.Select(p => new PessoaFisicaDto(
-                p.Id,
-                p.Nome,
-                p.Sobrenome,
-                p.Cpf,
-                p.DataNascimento,
-                p.Enderecos.Select(e => new EnderecoDto(
-                    e.Endereco.Id,
-                    e.Endereco.Cep,
-                    e.Endereco.Logradouro,
-                    e.Endereco.Complemento,
-                    e.Endereco.Unidade,
-                    e.Endereco.Bairro,
-                    e.Endereco.Localidade,
-                    e.Endereco.Uf,
-                    e.Endereco.Estado,
-                    e.Endereco.Regiao,
-                    e.Endereco.Ddd
-                )).ToList()
-            ));
+                return pessoasFisica.Select(p => new PessoaFisicaDto(
+                    p.Id,
+                    p.Nome,
+                    p.Sobrenome,
+                    p.Cpf,
+                    p.DataNascimento,
+                    p.Enderecos.Select(e => new EnderecoDto(
+                        e.Endereco.Id,
+                        e.Endereco.Cep,
+                        e.Endereco.Logradouro,
+                        e.Endereco.Complemento,
+                        e.Endereco.Unidade,
+                        e.Endereco.Bairro,
+                        e.Endereco.Localidade,
+                        e.Endereco.Uf,
+                        e.Endereco.Estado,
+                        e.Endereco.Regiao,
+                        e.Endereco.Ddd
+                    )).ToList()
+                ));
+            } catch (Exception ex)
+            {
+                _logger.LogError($"Error to find pessoa física by name: {name}", ex);
+                throw;
+            }
         }
 
         public async Task<PessoaFisicaDto> Create(CreatePessoaFisicaDto dto)
         {
-            var pessoaFisica = new PessoaFisica(
-                dto.Nome,
-                dto.Sobrenome,
-                dto.Cpf,
-                dto.DataNascimento
-            );
+            try
+            {
+                var pessoaFisica = new PessoaFisica(
+                    dto.Nome,
+                    dto.Sobrenome,
+                    dto.Cpf,
+                    dto.DataNascimento
+                );
 
-            var enderecoDto = await _viaCepService.FindAddressByCep(dto.Cep);
+                var enderecoDto = await _viaCepService.FindAddressByCep(dto.Cep);
 
-            var endereco = new Endereco(
-                cep: enderecoDto.Cep,
-                logradouro: enderecoDto.Logradouro,
-                complemento: enderecoDto.Complemento,
-                unidade: enderecoDto.Unidade ?? "",
-                bairro: enderecoDto.Bairro,
-                localidade: enderecoDto.Localidade,
-                uf: enderecoDto.Uf,
-                estado: enderecoDto.Estado ?? "",
-                regiao: enderecoDto.Regiao ?? "",
-                ddd: enderecoDto.Ddd ?? ""
-            );
+                var endereco = new Endereco(
+                    cep: enderecoDto.Cep,
+                    logradouro: enderecoDto.Logradouro,
+                    complemento: enderecoDto.Complemento,
+                    unidade: enderecoDto.Unidade ?? "",
+                    bairro: enderecoDto.Bairro,
+                    localidade: enderecoDto.Localidade,
+                    uf: enderecoDto.Uf,
+                    estado: enderecoDto.Estado ?? "",
+                    regiao: enderecoDto.Regiao ?? "",
+                    ddd: enderecoDto.Ddd ?? ""
+                );
 
-            var pessoaEndereco = new PessoaFisicaEndereco(
-                pessoaFisica,
-                endereco
-            );
+                var pessoaEndereco = new PessoaFisicaEndereco(
+                    pessoaFisica,
+                    endereco
+                );
 
-            pessoaFisica.Enderecos.Add(pessoaEndereco);
-            await _repository.Create(pessoaFisica);
+                pessoaFisica.Enderecos.Add(pessoaEndereco);
+                await _repository.Create(pessoaFisica);
 
-            return new PessoaFisicaDto(
-                pessoaFisica.Id,
-                pessoaFisica.Nome,
-                pessoaFisica.Sobrenome,
-                pessoaFisica.Cpf,
-                pessoaFisica.DataNascimento,
-                pessoaFisica.Enderecos.Select(e => new EnderecoDto(
-                    e.Endereco.Id,
-                    e.Endereco.Cep,
-                    e.Endereco.Logradouro,
-                    e.Endereco.Complemento,
-                    e.Endereco.Unidade,
-                    e.Endereco.Bairro,
-                    e.Endereco.Localidade,
-                    e.Endereco.Uf,
-                    e.Endereco.Estado,
-                    e.Endereco.Regiao,
-                    e.Endereco.Ddd
-                )).ToList()
-            );
+                return new PessoaFisicaDto(
+                    pessoaFisica.Id,
+                    pessoaFisica.Nome,
+                    pessoaFisica.Sobrenome,
+                    pessoaFisica.Cpf,
+                    pessoaFisica.DataNascimento,
+                    pessoaFisica.Enderecos.Select(e => new EnderecoDto(
+                        e.Endereco.Id,
+                        e.Endereco.Cep,
+                        e.Endereco.Logradouro,
+                        e.Endereco.Complemento,
+                        e.Endereco.Unidade,
+                        e.Endereco.Bairro,
+                        e.Endereco.Localidade,
+                        e.Endereco.Uf,
+                        e.Endereco.Estado,
+                        e.Endereco.Regiao,
+                        e.Endereco.Ddd
+                    )).ToList()
+                );
+
+            } catch (Exception ex)
+            {
+                _logger.LogError($"Error to create pessoa física entity. payload: {dto}", ex);
+                throw;
+            }
         }
 
         public async Task<PessoaFisicaDto?> UpdateById(Guid id, UpdatePessoaFisicaDto dto)
         {
-            var pessoaFisica = await _repository.FindById(id);
-            if (pessoaFisica == null) return null;
+            try
+            {
+                var pessoaFisica = await _repository.FindById(id);
+                if (pessoaFisica == null) return null;
 
-            pessoaFisica.Update(
-                dto.Nome,
-                dto.Sobrenome,
-                dto.DataNascimento,
-                dto.Cpf
-            );
+                pessoaFisica.Update(
+                    dto.Nome,
+                    dto.Sobrenome,
+                    dto.DataNascimento,
+                    dto.Cpf
+                );
 
-            await _repository.UpdateById(id, pessoaFisica);
+                await _repository.UpdateById(id, pessoaFisica);
 
-            return new PessoaFisicaDto(
-                pessoaFisica.Id,
-                pessoaFisica.Nome,
-                pessoaFisica.Sobrenome,
-                pessoaFisica.Cpf,
-                pessoaFisica.DataNascimento,
-                pessoaFisica.Enderecos.Select(e => new EnderecoDto(
-                    e.Endereco.Id,
-                    e.Endereco.Cep,
-                    e.Endereco.Logradouro,
-                    e.Endereco.Complemento,
-                    e.Endereco.Unidade,
-                    e.Endereco.Bairro,
-                    e.Endereco.Localidade,
-                    e.Endereco.Uf,
-                    e.Endereco.Estado,
-                    e.Endereco.Regiao,
-                    e.Endereco.Ddd
-                )).ToList()
-            );
+                return new PessoaFisicaDto(
+                    pessoaFisica.Id,
+                    pessoaFisica.Nome,
+                    pessoaFisica.Sobrenome,
+                    pessoaFisica.Cpf,
+                    pessoaFisica.DataNascimento,
+                    pessoaFisica.Enderecos.Select(e => new EnderecoDto(
+                        e.Endereco.Id,
+                        e.Endereco.Cep,
+                        e.Endereco.Logradouro,
+                        e.Endereco.Complemento,
+                        e.Endereco.Unidade,
+                        e.Endereco.Bairro,
+                        e.Endereco.Localidade,
+                        e.Endereco.Uf,
+                        e.Endereco.Estado,
+                        e.Endereco.Regiao,
+                        e.Endereco.Ddd
+                    )).ToList()
+                );
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Error to update pessoa física entity. payload: {dto}", ex);
+                throw;
+            }
+
         }
     }
 }
--- Application/Services/PessoaJuridicaService.cs
+++ Application/Services/PessoaJuridicaService.cs
@@ -2,6 +2,7 @@
 using Application.Services.Interfaces;
 using Domain.Entities;
 using Infrastructure.Repositories.Interfaces;
+using Microsoft.Extensions.Logging;
 
 
 namespace Application.Services
@@ -9,177 +10,212 @@ namespace Application.Services
     public class PessoaJuridicaService : BaseService<PessoaJuridica>, IPessoaJuridicaService
     {
 
+        private readonly ILogger<PessoaJuridica> _logger;
         private readonly IPessoaJuridicaRepository _repository;
         private readonly IViaCepService _viaCepService;
 
         public PessoaJuridicaService(
+            ILogger<PessoaJuridica> logger,
             IPessoaJuridicaRepository repository,
             IViaCepService viaCepService)
             : base(repository)
         {
+            _logger = logger;
             _repository = repository;
             _viaCepService = viaCepService;
         }
 
         public async Task<PessoaJuridicaDto?> FindByCnpj(string cnpj)
         {
-            var pessoaJuridica = await _repository.FindByCnpj(cnpj);
+            try
+            {
+                var pessoaJuridica = await _repository.FindByCnpj(cnpj);
 
-            if (pessoaJuridica == null)
-                return null;
+                if (pessoaJuridica == null)
+                    return null;
 
-            return new PessoaJuridicaDto(
-                pessoaJuridica.Id,
-                pessoaJuridica.RazaoSocial,
-                pessoaJuridica.NomeFantasia,
-                pessoaJuridica.Cnpj,
-                new EnderecoDto(
-                    pessoaJuridica.Endereco.Id,
-                    pessoaJuridica.Endereco.Cep,
-                    pessoaJuridica.Endereco.Logradouro,
-                    pessoaJuridica.Endereco.Complemento,
-                    pessoaJuridica.Endereco.Unidade,
-                    pessoaJuridica.Endereco.Bairro,
-                    pessoaJuridica.Endereco.Localidade,
-                    pessoaJuridica.Endereco.Uf,
-                    pessoaJuridica.Endereco.Estado,
-                    pessoaJuridica.Endereco.Regiao,
-                    pessoaJuridica.Endereco.Ddd
-                )
-            );
+                return new PessoaJuridicaDto(
+                    pessoaJuridica.Id,
+                    pessoaJuridica.RazaoSocial,
+                    pessoaJuridica.NomeFantasia,
+                    pessoaJuridica.Cnpj,
+                    new EnderecoDto(
+                        pessoaJuridica.Endereco.Id,
+                        pessoaJuridica.Endereco.Cep,
+                        pessoaJuridica.Endereco.Logradouro,
+                        pessoaJuridica.Endereco.Complemento,
+                        pessoaJuridica.Endereco.Unidade,
+                        pessoaJuridica.Endereco.Bairro,
+                        pessoaJuridica.Endereco.Localidade,
+                        pessoaJuridica.Endereco.Uf,
+                        pessoaJuridica.Endereco.Estado,
+                        pessoaJuridica.Endereco.Regiao,
+                        pessoaJuridica.Endereco.Ddd
+                    )
+                );
+            } catch (Exception ex)
+            {
+                _logger.LogError($"Error to find Pessoa Jurídica by CNPJ: {cnpj}", ex);
+                throw;
+            }
         }
 
         public async Task<IEnumerable<PessoaJuridicaDto?>> FindByNomeFantasia(string nomeFantasia)
         {
-            var pessoas = await _repository.FindByNomeFantasia(nomeFantasia);
+            try
+            {
+                var pessoas = await _repository.FindByNomeFantasia(nomeFantasia);
 
-            if (pessoas == null)
-                return null;
+                if (pessoas == null)
+                    return null;
 
-            return pessoas.Select(p => new PessoaJuridicaDto(
-                p.Id,
-                p.RazaoSocial,
-                p.NomeFantasia,
-                p.Cnpj,
-                new EnderecoDto(
-                    p.Endereco.Id,
-                    p.Endereco.Cep,
-                    p.Endereco.Logradouro,
-                    p.Endereco.Complemento,
-                    p.Endereco.Unidade,
-                    p.Endereco.Bairro,
-                    p.Endereco.Localidade,
-                    p.Endereco.Uf,
-                    p.Endereco.Estado,
-                    p.Endereco.Regiao,
-                    p.Endereco.Ddd
-                )
-            ));
+                return pessoas.Select(p => new PessoaJuridicaDto(
+                    p.Id,
+                    p.RazaoSocial,
+                    p.NomeFantasia,
+                    p.Cnpj,
+                    new EnderecoDto(
+                        p.Endereco.Id,
+                        p.Endereco.Cep,
+                        p.Endereco.Logradouro,
+                        p.Endereco.Complemento,
+                        p.Endereco.Unidade,
+                        p.Endereco.Bairro,
+                        p.Endereco.Localidade,
+                        p.Endereco.Uf,
+                        p.Endereco.Estado,
+                        p.Endereco.Regiao,
+                        p.Endereco.Ddd
+                    )
+                ));
+            } catch (Exception ex)
+            {
+                _logger.LogError($"Error to find Pessoa Jurídica by nome fantasia: {nomeFantasia}", ex);
+                throw;
+            }
         }
 
         public async Task<IEnumerable<PessoaJuridicaDto?>> FindByRazaoSocial(string razaoSocial)
         {
-            var pessoas = await _repository.FindByRazaoSocial(razaoSocial);
+            try
+            {
+                var pessoas = await _repository.FindByRazaoSocial(razaoSocial);
 
-            if (pessoas == null)
-                return null;
+                if (pessoas == null)
+                    return null;
 
-            return pessoas.Select(p => new PessoaJuridicaDto(
-                p.Id,
-                p.RazaoSocial,
-                p.NomeFantasia,
-                p.Cnpj,
-                new EnderecoDto(
-                    p.Endereco.Id,
-                    p.Endereco.Cep,
-                    p.Endereco.Logradouro,
-                    p.Endereco.Complemento,
-                    p.Endereco.Unidade,
-                    p.Endereco.Bairro,
-                    p.Endereco.Localidade,
-                    p.Endereco.Uf,
-                    p.Endereco.Estado,
-                    p.Endereco.Regiao,
-                    p.Endereco.Ddd
-                )
-            ));
+                return pessoas.Select(p => new PessoaJuridicaDto(
+                    p.Id,
+                    p.RazaoSocial,
+                    p.NomeFantasia,
+                    p.Cnpj,
+                    new EnderecoDto(
+                        p.Endereco.Id,
+                        p.Endereco.Cep,
+                        p.Endereco.Logradouro,
+                        p.Endereco.Complemento,
+                        p.Endereco.Unidade,
+                        p.Endereco.Bairro,
+                        p.Endereco.Localidade,
+                        p.Endereco.Uf,
+                        p.Endereco.Estado,
+                        p.Endereco.Regiao,
+                        p.Endereco.Ddd
+                    )
+                ));
+
+            } catch (Exception ex)
+            {
+                _logger.LogError($"Error to find Pessoa Jurídica by razão social: {razaoSocial}", ex);
+                throw;
+            }
         }
 
         public async Task<PessoaJuridicaDto> Create(CreatePessoaJuridicaDto dto)
         {
-            var existing = await _repository.FindByCnpj(dto.Cnpj);
-            if (existing != null)
-                throw new Exception($"CNPJ {dto.Cnpj} já cadastrado.");
+            try
+            {
+                var existing = await _repository.FindByCnpj(dto.Cnpj);
+                if (existing != null)
+                    throw new Exception($"CNPJ {dto.Cnpj} já cadastrado.");
 
-            var enderecoDto = await _viaCepService.FindAddressByCep(dto.Cep);
-            if (enderecoDto == null)
-                throw new Exception($"CEP não encontrado: {dto.Cep}");
+                var enderecoDto = await _viaCepService.FindAddressByCep(dto.Cep);
+                if (enderecoDto == null)
+                    throw new Exception($"CEP não encontrado: {dto.Cep}");
 
-            var endereco = new Endereco(
-                cep: enderecoDto.Cep,
-                logradouro: enderecoDto.Logradouro,
-                complemento: enderecoDto.Complemento,
-                unidade: enderecoDto.Unidade ?? "",
-                bairro: enderecoDto.Bairro,
-                localidade: enderecoDto.Localidade,
-                uf: enderecoDto.Uf,
-                estado: enderecoDto.Estado ?? "",
-                regiao: enderecoDto.Regiao ?? "",
-                ddd: enderecoDto.Ddd ?? ""
-            );
+                var endereco = new Endereco(
+                    cep: enderecoDto.Cep,
+                    logradouro: enderecoDto.Logradouro,
+                    complemento: enderecoDto.Complemento,
+                    unidade: enderecoDto.Unidade ?? "",
+                    bairro: enderecoDto.Bairro,
+                    localidade: enderecoDto.Localidade,
+                    uf: enderecoDto.Uf,
+                    estado: enderecoDto.Estado ?? "",
+                    regiao: enderecoDto.Regiao ?? "",
+                    ddd: enderecoDto.Ddd ?? ""
+                );
 
-            var pessoaJuridica = new PessoaJuridica(
-                razaoSocial: dto.RazaoSocial,
-                nomeFantasia: dto.NomeFantasia,
-                cnpj: dto.Cnpj,
-                endereco: endereco
-            );
+                var pessoaJuridica = new PessoaJuridica(
+                    razaoSocial: dto.RazaoSocial,
+                    nomeFantasia: dto.NomeFantasia,
+                    cnpj: dto.Cnpj,
+                    endereco: endereco
+                );
 
-            await _repository.Create(pessoaJuridica);
-            
-            return new PessoaJuridicaDto(
-                pessoaJuridica.Id,
-                pessoaJuridica.RazaoSocial,
-                pessoaJuridica.NomeFantasia,
-                pessoaJuridica.Cnpj,
-                new EnderecoDto(
-                    endereco.Id,
-                    endereco.Cep,
-                    endereco.Logradouro,
-                    endereco.Complemento,
-                    endereco.Unidade,
-                    endereco.Bairro,
-                    endereco.Localidade,
-                    endereco.Uf,
-                    endereco.Estado,
-                    endereco.Regiao,
-                    endereco.Ddd
-                )
-            );
+                await _repository.Create(pessoaJuridica);
+
+                return new PessoaJuridicaDto(
+                    pessoaJuridica.Id,
+                    pessoaJuridica.RazaoSocial,
+                    pessoaJuridica.NomeFantasia,
+                    pessoaJuridica.Cnpj,
+                    new EnderecoDto(
+                        endereco.Id,
+                        endereco.Cep,
+                        endereco.Logradouro,
+                        endereco.Complemento,
+                        endereco.Unidade,
+                        endereco.Bairro,
+                        endereco.Localidade,
+                        endereco.Uf,
+                        endereco.Estado,
+                        endereco.Regiao,
+                        endereco.Ddd
+                    )
+                );
+            } catch (Exception ex)
+            {
+                _logger.LogError($"Error to create Pessoa Jurídica. Payload: {dto}", ex);
+                throw;
+            }
         }
 
-        public async Task<UpdatePessoaJuridicaDto?> UpdateById(
-            Guid id,
-            UpdatePessoaJuridicaDto dto)
+        public async Task<UpdatePessoaJuridicaDto?> UpdateById(Guid id,UpdatePessoaJuridicaDto dto)
         {
-            var pessoaJuridica = await _repository.FindById(id);
-            if (pessoaJuridica == null)
-                return null;
+            try {
+                var pessoaJuridica = await _repository.FindById(id);
+                if (pessoaJuridica == null)
+                    return null;
 
-            pessoaJuridica.Update(
-                dto.RazaoSocial,
-                dto.NomeFantasia,
-                dto.Cnpj
-            );
+                pessoaJuridica.Update(
+                    dto.RazaoSocial,
+                    dto.NomeFantasia,
+                    dto.Cnpj
+                );
 
-            await _repository.UpdateById(id, pessoaJuridica);
+                await _repository.UpdateById(id, pessoaJuridica);
 
-            return new UpdatePessoaJuridicaDto(
-                pessoaJuridica.RazaoSocial,
-                pessoaJuridica.NomeFantasia,
-                pessoaJuridica.Cnpj
-            );
+                return new UpdatePessoaJuridicaDto(
+                    pessoaJuridica.RazaoSocial,
+                    pessoaJuridica.NomeFantasia,
+                    pessoaJuridica.Cnpj
+                );
+            } catch (Exception ex)
+            {
+                _logger.LogError($"Error to update Pessoa Jurídica id: {id}. Payload: {dto}", ex);
+                throw;
+            }
         }
-
     }
 }
