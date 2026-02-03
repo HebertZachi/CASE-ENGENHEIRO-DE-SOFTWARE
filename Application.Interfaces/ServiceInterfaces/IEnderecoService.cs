cd867121f8050d1f134097da08129de976d553c4
Hebert Zachi <hebertzachi.contato@outlook.com>
2026-02-03 00:01:58 -0300

    :recycle: refactor: change types methods and added create and update by id

--- Application.Interfaces/ServiceInterfaces/IEnderecoService.cs
+++ Application.Interfaces/ServiceInterfaces/IEnderecoService.cs
@@ -1,10 +1,13 @@
-﻿using Domain.Entities;
+﻿using Application.DTO;
+using Domain.Entities;
 
 namespace Application.Services.Interfaces
 {
     public interface IEnderecoService : IService<Endereco>
     {
-        Task<IEnumerable<Endereco>> FindByCep(string cep, int page = 1, int limit = 10);
-        Task<IEnumerable<Endereco>> FindByLocalidade(string localidade, int page = 1, int limit = 10);
+        Task<IEnumerable<EnderecoDto>> FindByCep(string cep, int page = 1, int limit = 10);
+        Task<IEnumerable<EnderecoDto>> FindByLocalidade(string localidade, int page = 1, int limit = 10);
+        Task<EnderecoDto> Create(CreateEnderecoDto dto);
+        Task<EnderecoDto?> UpdateById(Guid id, UpdateEnderecoDto dto);
     }
 }
