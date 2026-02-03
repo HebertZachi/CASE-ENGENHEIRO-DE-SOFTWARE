9c75b38564aa2d76844f47adf30857229de059fa
Hebert Zachi <hebertzachi.contato@outlook.com>
2026-02-02 23:58:26 -0300

    :sparkles: feat: added new methods for enderecos

--- Application/Services/EnderecoService.cs
+++ Application/Services/EnderecoService.cs
@@ -2,23 +2,171 @@
 using Application.Services.Interfaces;
 using Domain.Entities;
 using Infrastructure.Repositories.Interfaces;
-
+using Microsoft.Extensions.Logging;
 
 namespace Application.Services
 {
     public class EnderecoService : BaseService<Endereco>, IEnderecoService
     {
+        private readonly ILogger<EnderecoService> _logger;
         private readonly IEnderecoRepository _repository;
-        public EnderecoService(IEnderecoRepository repository, IGenericRepository<Endereco> genericRepository)
-            : base(genericRepository)
+        private readonly IViaCepService _viaCepService;
+
+        public EnderecoService(
+            ILogger<EnderecoService> logger,
+            IEnderecoRepository repository,
+            IViaCepService viaCepService
+        ) : base(repository)
         {
+            _logger = logger;
             _repository = repository;
+            _viaCepService = viaCepService;
         }
 
-        public async Task<IEnumerable<Endereco>> FindByCep(string cep, int page = 1, int limit = 10) => 
-            await _repository.FindByCep(cep, page, limit);
+        public async Task<IEnumerable<EnderecoDto?>> FindByCep(string cep, int page = 1, int limit = 10)
+        {
+            try
+            {
+                var enderecos = await _repository.FindByCep(cep, page, limit);
 
-        public async Task<IEnumerable<Endereco>> FindByLocalidade(string localidade, int page = 1, int limit = 10) =>
-            await _repository.FindByLocalidade(localidade, page, limit);
+                if (enderecos == null)
+                    return null;
+
+                return enderecos.Select(e => new EnderecoDto(
+                    e.Id,
+                    e.Cep,
+                    e.Logradouro,
+                    e.Complemento,
+                    e.Unidade,
+                    e.Bairro,
+                    e.Localidade,
+                    e.Uf,
+                    e.Estado,
+                    e.Regiao,
+                    e.Ddd
+                ));
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Error to find Endereco by CEP: {cep}", ex);
+                throw;
+            }
+        }
+
+        public async Task<IEnumerable<EnderecoDto?>> FindByLocalidade(string localidade, int page = 1, int limit = 10)
+        {
+            try
+            {
+                var enderecos = await _repository.FindByLocalidade(localidade, page, limit);
+
+                if (enderecos == null)
+                    return null;
+
+                return enderecos.Select(e => new EnderecoDto(
+                    e.Id,
+                    e.Cep,
+                    e.Logradouro,
+                    e.Complemento,
+                    e.Unidade,
+                    e.Bairro,
+                    e.Localidade,
+                    e.Uf,
+                    e.Estado,
+                    e.Regiao,
+                    e.Ddd
+                ));
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Error to find Endereco by Localidade: {localidade}", ex);
+                throw;
+            }
+        }
+
+        public async Task<EnderecoDto> Create(CreateEnderecoDto dto)
+        {
+            try
+            {
+                var enderecoDto = await _viaCepService.FindAddressByCep(dto.Cep);
+
+                var endereco = new Endereco(
+                    cep: enderecoDto.Cep,
+                    logradouro: enderecoDto.Logradouro,
+                    complemento: enderecoDto.Complemento,
+                    unidade: enderecoDto.Unidade ?? "",
+                    bairro: enderecoDto.Bairro,
+                    localidade: enderecoDto.Localidade,
+                    uf: enderecoDto.Uf,
+                    estado: enderecoDto.Estado ?? "",
+                    regiao: enderecoDto.Regiao ?? "",
+                    ddd: enderecoDto.Ddd ?? ""
+                );
+
+                await _repository.Create(endereco);
+
+                return new EnderecoDto(
+                    endereco.Id,
+                    endereco.Cep,
+                    endereco.Logradouro,
+                    endereco.Complemento,
+                    endereco.Unidade,
+                    endereco.Bairro,
+                    endereco.Localidade,
+                    endereco.Uf,
+                    endereco.Estado,
+                    endereco.Regiao,
+                    endereco.Ddd
+                );
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Error to create endereço entity. payload: {dto}", ex);
+                throw;
+            }
+        }
+
+        public async Task<EnderecoDto?> UpdateById(Guid id, UpdateEnderecoDto dto)
+        {
+            try
+            {
+                var endereco = await _repository.FindById(id);
+                if (endereco == null)
+                    return null;
+
+                endereco.Update(
+                    dto.cep,
+                    dto.Logradouro,
+                    dto.Complemento,
+                    dto.Unidade,
+                    dto.Bairro,
+                    dto.Localidade,
+                    dto.Uf,
+                    dto.Estado,
+                    dto.Regiao,
+                    dto.Ddd
+                );
+
+                await _repository.UpdateById(id, endereco);
+
+                return new EnderecoDto(
+                    endereco.Id,
+                    endereco.Cep,
+                    endereco.Logradouro,
+                    endereco.Complemento,
+                    endereco.Unidade,
+                    endereco.Bairro,
+                    endereco.Localidade,
+                    endereco.Uf,
+                    endereco.Estado,
+                    endereco.Regiao,
+                    endereco.Ddd
+                );
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Error to update endereço entity. payload: {dto}", ex);
+                throw;
+            }
+        }
     }
 }
